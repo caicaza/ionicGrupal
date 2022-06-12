d8fb34eeec3d086dea60f63a66b4d4fb766f79d7
Gcaicaza <carlosicazaguamba2@gmail.com>
2022-06-12 14:26:51 -0500

    Form Group y cambios
    Modificación en el grid de platillos e importacion de FormGroup de Login y Registro

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -53,6 +53,15 @@ const routes: Routes = [
         ],
       },
       {
+        path: 'registro',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../registro/registro.module').then( m => m.RegistroPageModule)
+          }
+        ],
+      },
+      {
         path: '',
         redirectTo: '/inicio/platillos',
         pathMatch: 'full'
